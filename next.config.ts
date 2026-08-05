5da1b0937ddf12bf844855fd6c67bdbe2bd77233
Z User <z@container>
2026-08-05 18:48:39 +0000

    fix: use next.config.ts with DATABASE_URL in env

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL || "file:/tmp/haraj.db",
+  },
 };
 
 export default nextConfig;
